db7edef8b0d1bb92478960dd780e818aa15b489e
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-12 14:53:04 +0100

    cambio

--- test/menu.py
+++ test/menu.py
@@ -1,46 +1,58 @@
-import os
-import sys
-import re
+import math
 import helpers
 from ejercicio1 import Punto
-from ejercicio1 import Rectangulo 
+from ejercicio1 import Rectangulo
 
-
-
-
-def iniciar():
+def menu():
     while True:
         helpers.limpiar_pantalla()
 
-        lineas=("="*50)
-        print(lineas)
-        print(((("Bienvenido al menú de ejercicios de Python").upper()).center(50)))
-        print(lineas)
-        print(("[1]"),"Ejercicio 1: Punto")
-        print(("[2]"),"Ejercicio 2: Rectángulo")
-        print(("[8]"),("SALIR"))
-        print(lineas)
-
-        opcion = input(("Elige una opción: "))
+        print("="*50)
+        print("Bienvenido al menú de ejercicios de Python".upper().center(50))
+        print("="*50)
+        print("[1] Ejercicio 1: Punto")
+        print("[2] Ejercicio 2: Rectángulo")
+        print("[3] Salir")
+        opcion = input("Elige una opción: ")
         helpers.limpiar_pantalla()
+
+        print("="*50)
 
         if opcion == "1":
             print("Ejercicio 1: Punto")
-            #ejercicicios.alumno
-            Punto=input(("Ingresa un punto con cooredenadas x e y: "))
-            print(Punto)
-
-        
+            x = float(input("Ingrese la coordenada x: "))
+            y = float(input("Ingrese la coordenada y: "))
+            punto = Punto(x, y)
+            print(punto)
+            print(punto.cuadrante())
+            x2 = float(input("Ingrese la coordenada x del segundo punto: "))
+            y2 = float(input("Ingrese la coordenada y del segundo punto: "))
+            punto2 = Punto(x2, y2)
+            print(punto.vector(punto2))
+            print(punto.distancia(punto2))
 
         elif opcion == "2":
             print("Ejercicio 2: Rectángulo")
-            n=int(input(("Ingresa un punto: ")))
-            print(Rectangulo(n))
+            x1 = float(input("Ingrese la coordenada x del primer punto: "))
+            y1 = float(input("Ingrese la coordenada y del primer punto: "))
+            punto1 = Punto(x1, y1)
+            x2 = float(input("Ingrese la coordenada x del segundo punto: "))
+            y2 = float(input("Ingrese la coordenada y del segundo punto: "))
+            punto2 = Punto(x2, y2)
+            rectangulo = Rectangulo(punto1, punto2)
+            print(rectangulo.base())
+            print(rectangulo.altura())
+            print(rectangulo.area())
 
-        elif opcion == "8":
-            print((F"Saliendo..."))
+        elif opcion == "3":
+            print("Saliendo...")
             break
 
-        input((F">>> Presiona ENTER para continuar <<<"))
+        else:
+            print("Opción inválida. Intente de nuevo.")
+            print("="*50)
 
-(iniciar())
+        input("Presione ENTER para continuar...")
+        print("="*50)
+        
+menu()
